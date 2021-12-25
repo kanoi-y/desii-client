13b14f9526bd2f4c941c7828f0533eb5a2155ea4
kanoi-y <furafuratonbo@gmail.com>
2021-12-25 18:25:09 +0900

    color.tsを作成

--- src/components/parts/commons/Button/Button.tsx
+++ src/components/parts/commons/Button/Button.tsx
@@ -1,15 +1,18 @@
 import { Button as ChakraButton } from '@chakra-ui/react'
-import { ComponentProps, VFC } from 'react'
+import { VFC } from 'react'
+import { ColorVariables } from '~/constants/color'
 
-type Props = ComponentProps<typeof ChakraButton>
+type Props = {
+  color?: ColorVariables
+}
 
-export const Button: VFC<Props> = ({
-  colorScheme = 'white',
-  boxShadow = '0 3px 6px rgba(0, 0, 0, 0.16)',
-  color = 'textColor.main',
-  ...rest
-}) => {
+export const Button: VFC<Props> = ({ color = 'textColor.main', ...rest }) => {
   return (
-    <ChakraButton boxShadow={boxShadow} colorScheme={colorScheme} color={color} {...rest} />
+    <ChakraButton
+      boxShadow="0 3px 6px rgba(0, 0, 0, 0.16)"
+      colorScheme="white"
+      color={color}
+      {...rest}
+    />
   )
 }
---
+++ src/constants/color.ts
@@ -0,0 +1,1 @@
+export type ColorVariables = 'primary' | 'secondary' | 'info' | 'warning' | 'danger' | 'textColor.main';
