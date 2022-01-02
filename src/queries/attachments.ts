9993d606622218e85d6fe92cecd457cb7aa44b0f
kanoi-y <furafuratonbo@gmail.com>
2022-01-02 22:17:41 +0900

    ✨ queriesを作成

---
+++ src/queries/attachments.ts
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client'
+
+export const GET_ATTACHMENT_BY_ID = gql`
+  query GetAttachmentById($id: uuid!) {
+    attachments_by_pk(id: $id) {
+      _id
+      name
+      size
+      filePath
+      createdUserId
+      createdAt
+      updatedAt
+    }
+  }
+`
+
+export const CREATE_ATTACHMENT = gql`
+  mutation CreateAttachment(
+    $name: String!
+    $size: Number!
+    $filePath: String!
+    $createdUserId: String!
+  ) {
+    insert_attachments_one(
+      object: {
+        name: $name
+        size: $size
+        filePath: $filePath
+        createdUserId: $createdUserId
+      }
+    ) {
+      _id
+      name
+      size
+      filePath
+      createdUserId
+      createdAt
+      updatedAt
+    }
+  }
+`
+
+export const DELETE_ATTACHMENT = gql`
+  mutation DeleteAttachment($id: uuid!) {
+    delete_attachments_by_pk(id: $id) {
+      _id
+      name
+      size
+      filePath
+      createdUserId
+      createdAt
+      updatedAt
+    }
+  }
+`
---
+++ src/queries/users.ts
@@ -0,0 +1,98 @@
+import { gql } from '@apollo/client'
+
+export const GET_USERS = gql`
+  query GetUsers {
+    users {
+      _id
+      name
+      description
+      email
+      iconImageId
+      createdAt
+      updatedAt
+    }
+  }
+`
+
+export const GET_USER_BY_ID = gql`
+  query GetUserById($id: uuid!) {
+    users_by_pk(id: $id) {
+      _id
+      name
+      description
+      email
+      iconImageId
+      createdAt
+      updatedAt
+    }
+  }
+`
+
+export const CREATE_USER = gql`
+  mutation CreateUser(
+    $name: String!
+    $description: String!
+    $email: String!
+    $iconImageId: String!
+  ) {
+    insert_users_one(
+      object: {
+        name: $name
+        description: $description
+        email: $email
+        iconImageId: $iconImageId
+      }
+    ) {
+      _id
+      name
+      description
+      email
+      iconImageId
+      createdAt
+      updatedAt
+    }
+  }
+`
+
+export const DELETE_USER = gql`
+  mutation DeleteUser($id: uuid!) {
+    delete_users_by_pk(id: $id) {
+      _id
+      name
+      description
+      email
+      iconImageId
+      createdAt
+      updatedAt
+    }
+  }
+`
+
+export const UPDATE_USER = gql`
+  mutation UpdateUser(
+    $id: uuid!
+    $name: String
+    $description: String
+    $email: String
+    $iconImageId: String
+  ) {
+    update_users_by_pk(
+      pk_columns: { id: $id }
+      _set: {
+        name: $name
+        description: $description
+        email: $email
+        iconImageId: $iconImageId
+      }
+    ) {
+      _id
+      name
+      description
+      email
+      iconImageId
+      createdAt
+      updatedAt
+    }
+  }
+`
+
