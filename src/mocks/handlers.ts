0ac1b0a14ccccffb499e8268db84cfaabe625444
kanoi-y <furafuratonbo@gmail.com>
2022-01-04 12:00:51 +0900

    🎨 GetAttachmentByIdをmock

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,16 @@
-import { graphql } from 'msw';
+import { graphql } from 'msw'
+import { attachmentsFactory } from './factories/attachmentFactory'
 
-export const handlers = [];
+export const handlers = [
+  graphql.query('GetAttachmentById', (req, res, ctx) => {
+    const { id } = req.variables
+
+    return res(
+      ctx.data({
+        attachments_by_pk: {
+          ...attachmentsFactory({ _id: id }),
+        },
+      })
+    )
+  }),
+]
