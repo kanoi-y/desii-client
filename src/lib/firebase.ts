c101342cc2e362f1e172f0416fe4ddc1cd32ac84
kanoi-y <furafuratonbo@gmail.com>
2022-01-06 23:59:52 +0900

    🎨 firebase/storageを追加

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
+import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics'
-import { initializeApp } from 'firebase/app'
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
+export const storage = getStorage(app);
