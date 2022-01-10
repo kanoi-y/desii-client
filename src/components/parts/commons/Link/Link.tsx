c4591bb9c162e7d5c17ea9f3793acba6efafbbf4
kanoi-y <furafuratonbo@gmail.com>
2022-01-10 18:51:26 +0900

    ✨ Link.tsxを作成

---
+++ src/components/parts/commons/Link/Link.tsx
@@ -0,0 +1,17 @@
+import { Link as ChakraLink } from '@chakra-ui/react'
+import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+import { ReactNode, VFC } from 'react'
+
+type Props = {
+  children: ReactNode
+  href: NextLinkProps['href']
+  target?: string;
+}
+
+export const Link: VFC<Props> = ({ children, href, target }) => {
+  return (
+    <NextLink href={href} passHref>
+      <ChakraLink target={target || '_self'} rel={target ? 'opener noreferrer' : ''}>{children}</ChakraLink>
+    </NextLink>
+  )
+}
