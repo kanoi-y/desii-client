bed8a62897e4f6f01ce801d25a4bf1b9ef280913
kanoi-y <furafuratonbo@gmail.com>
2022-01-11 22:17:09 +0900

    🐛 functionsディレクトリのeslintとprettierを設定

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -11,21 +11,21 @@ module.exports = {
     "plugin:import/typescript",
     "google",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
+    tsconfigRootDir: __dirname,
     sourceType: "module",
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
+    "@typescript-eslint/explicit-function-return-type": "warn",
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,61 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import ApolloClient from "apollo-boost";
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+import gql from "graphql-tag";
+import fetch from "node-fetch";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const client = new ApolloClient({
+  uri: functions.config().hasura.url,
+  fetch: fetch as any,
+  request: (operation: any): void => {
+    operation.setContext({
+      headers: {
+        "x-hasura-admin-secret": functions.config().hasura.admin_secret,
+      },
+    });
+  },
+});
+
+export const setCustomClaims = functions.auth.user().onCreate(async (user) => {
+  // Hasuraの検証用のカスタムクレーム（属性情報）
+  const customClaims = {
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "user",
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": user.uid,
+    },
+  };
+
+  try {
+    // カスタムクレームの設定
+    await admin.auth().setCustomUserClaims(user.uid, customClaims);
+
+    // Hasuraサーバーへのユーザーデータの作成リクエスト
+    await client.mutate({
+      variables: { id: user.uid, name: user.displayName || "unknown" },
+      mutation: gql`
+        mutation InsertUsers($id: String, $name: String) {
+          insert_users(objects: { id: $id, name: $name }) {
+            returning {
+              id
+              name
+              created_at
+            }
+          }
+        }
+      `,
+    });
+
+    // 初回ログインの際にユーザー作成と、カスタムクレームの設定には遅延があるため、
+    // tokenリフレッシュのフック用にFirestoreへのmetaデータ追加を行う
+    await admin.firestore().collection("user_meta").doc(user.uid).create({
+      refreshTime: admin.firestore.FieldValue.serverTimestamp(),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
