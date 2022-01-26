d6cb71e9a39b54b827e9b6b75249d4be2364fa02
kanoi-y <furafuratonbo@gmail.com>
2022-01-26 22:27:28 +0900

    🎨userのQueryとmutateを実装

--- src/graphql/types/User.ts
+++ src/graphql/types/User.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from 'nexus'
+import { extendType, nonNull, objectType, stringArg } from 'nexus'
 
 export const User = objectType({
   name: 'User',
@@ -11,7 +11,6 @@ export const User = objectType({
     })
     t.string('description')
     t.string('image')
-    t.string('iconImageId')
     t.nonNull.field('createdAt', {
       type: 'DateTime',
     })
@@ -28,6 +27,98 @@ export const UsersQuery = extendType({
       type: 'User',
       resolve(_parent, _args, ctx) {
         return ctx.prisma.user.findMany()
+      },
+    })
+  },
+})
+
+export const GetUserQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.field('getUser', {
+      type: 'User',
+      args: {
+        id: nonNull(stringArg()),
+      },
+      resolve(_parent, args, ctx) {
+        return ctx.prisma.user.findUnique({
+          where: {
+            id: args.id,
+          },
+        })
+      },
+    })
+  },
+})
+
+export const CreateUserMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('createUser', {
+      type: 'User',
+      args: {
+        name: nonNull(stringArg()),
+        email: nonNull(stringArg()),
+        description: stringArg(),
+        image: stringArg(),
+      },
+      resolve(_parent, args, ctx) {
+        return ctx.prisma.user.create({
+          data: {
+            name: args.name,
+            email: args.email,
+            description: args.description,
+            image: args.image,
+          },
+        })
+      },
+    })
+  },
+})
+
+export const DeleteUserMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('deleteUser', {
+      type: 'User',
+      args: {
+        id: nonNull(stringArg()),
+      },
+      resolve(_parent, args, ctx) {
+        return ctx.prisma.user.delete({
+          where: {
+            id: args.id,
+          },
+        })
+      },
+    })
+  },
+})
+
+export const UpdateUserMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('updateUser', {
+      type: 'User',
+      args: {
+        id: nonNull(stringArg()),
+        name: nonNull(stringArg()),
+        email: nonNull(stringArg()),
+        description: stringArg(),
+        image: stringArg(),
+      },
+      resolve(_parent, args, ctx) {
+        return ctx.prisma.user.update({
+          where: {
+            id: args.id,
+          },
+          data: {
+            name: args.name,
+            email: args.email,
+            description: args.description,
+            image: args.image,
+          },
+        })
       },
     })
   },
