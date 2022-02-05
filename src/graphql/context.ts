95a52addef2f67fc661b9e7f8b0c6b3041ecefe8
kanoi-y <furafuratonbo@gmail.com>
2022-02-05 22:05:10 +0900

    🔨 session情報をcontentに持たせるように実装

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,12 +1,22 @@
 import { PrismaClient } from '@prisma/client'
+import { MicroRequest } from 'apollo-server-micro/dist/types'
+import { Session } from 'next-auth'
+import { getSession } from 'next-auth/react'
 import { prisma } from '../lib/prisma'
 
 export type Context = {
   prisma: PrismaClient
+  session: Session | null
 }
 
-export async function createContext(): Promise<Context> {
+export async function createContext({
+  req,
+}: {
+  req: MicroRequest
+}): Promise<Context> {
+  const session = await getSession({ req })
   return {
     prisma,
+    session,
   }
 }
