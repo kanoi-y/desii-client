ab0740aee77aea0a274b6c7a072d40c68318b8a8
kanoi-y <furafuratonbo@gmail.com>
2022-02-14 23:18:02 +0900

    ✨ [productId]/index.tsxを作成

---
+++ src/pages/[productId]/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+export default function GroupPage() {
+  return (
+    <Box textAlign="center">
+      <p>group page</p>
+    </Box>
+  )
+}
