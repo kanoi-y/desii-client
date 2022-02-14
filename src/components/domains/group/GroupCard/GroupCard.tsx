5bef98d24124f8cef66e71bc55c59249ec509580
kanoi-y <furafuratonbo@gmail.com>
2022-02-14 23:12:44 +0900

    🎨 GroupCardにLinkを追加

--- src/components/domains/group/GroupCard/GroupCard.tsx
+++ src/components/domains/group/GroupCard/GroupCard.tsx
@@ -1,6 +1,6 @@
 import { Box, Image } from '@chakra-ui/react'
 import { VFC } from 'react'
-import { Text } from '~/components/parts/commons'
+import { Link, Text } from '~/components/parts/commons'
 import { Group, useGetUserGroupRelationsQuery } from '~/types/generated/graphql'
 
 type Props = {
@@ -15,23 +15,25 @@ export const GroupCard: VFC<Props> = ({ group }) => {
   })
 
   return (
-    <Box>
-      <Image src={group.image} alt={group.name} />
+    <Link href={`/${group.productId}`}>
       <Box>
-        <Box display="flex" alignItems="center">
-          <Text fontSize="lg" noOfLines={1}>
-            {group.name}
-          </Text>
-          <Text fontSize="md">
-            {data?.GetUserGroupRelations
-              ? data.GetUserGroupRelations.length.toString() + '人'
-              : ''}
+        <Image src={group.image} alt={group.name} />
+        <Box>
+          <Box display="flex" alignItems="center">
+            <Text fontSize="lg" noOfLines={1}>
+              {group.name}
+            </Text>
+            <Text fontSize="md">
+              {data?.GetUserGroupRelations
+                ? data.GetUserGroupRelations.length.toString() + '人'
+                : ''}
+            </Text>
+          </Box>
+          <Text fontSize="md" noOfLines={2}>
+            {group.description || ''}
           </Text>
         </Box>
-        <Text fontSize="md" noOfLines={2}>
-          {group.description || ''}
-        </Text>
       </Box>
-    </Box>
+    </Link>
   )
 }
