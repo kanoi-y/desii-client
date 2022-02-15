2eee4d8be5d88d46bb96ee48485a623ec1c514e9
kanoi-y <furafuratonbo@gmail.com>
2022-02-15 16:16:05 +0900

    💄 GroupCardの見た目を調整

--- src/components/domains/group/GroupCard/GroupCard.tsx
+++ src/components/domains/group/GroupCard/GroupCard.tsx
@@ -16,20 +16,34 @@ export const GroupCard: VFC<Props> = ({ group }) => {
 
   return (
     <Link href={`/${group.productId}`}>
-      <Box>
-        <Image src={group.image} alt={group.name} />
-        <Box>
-          <Box display="flex" alignItems="center">
-            <Text fontSize="lg" noOfLines={1}>
+      <Box
+        borderRadius="lg"
+        overflow="hidden"
+        bgColor="white"
+        w="280px"
+        h="230px"
+        display="flex"
+        flexDirection="column"
+      >
+        <Image src={group.image} alt={group.name} flex="1" />
+        <Box bgColor="primary.main" p="4px 8px">
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            mb="4px"
+            gap="4px"
+          >
+            <Text fontSize="lg" noOfLines={1} isBold>
               {group.name}
             </Text>
-            <Text fontSize="md">
+            <Text fontSize="sm" isBold noWrap>
               {data?.GetUserGroupRelations
                 ? data.GetUserGroupRelations.length.toString() + '人'
                 : ''}
             </Text>
           </Box>
-          <Text fontSize="md" noOfLines={2}>
+          <Text fontSize="sm" noOfLines={2}>
             {group.description || ''}
           </Text>
         </Box>
