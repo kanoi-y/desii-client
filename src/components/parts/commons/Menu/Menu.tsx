a60967673b7d3983991b7382fbafc994f0810ef5
kanoi-y <furafuratonbo@gmail.com>
2022-02-19 16:47:13 +0900

    ✨ Menu.tsxを作成

---
+++ src/components/parts/commons/Menu/Menu.tsx
@@ -0,0 +1,31 @@
+import {
+  Menu as ChakraMenu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react'
+import { ReactElement, ReactNode, VFC } from 'react'
+
+type Props = {
+  toggleItem: ReactNode
+  menuList: { text: string; icon?: ReactElement; underline: boolean }[]
+}
+
+export const Menu: VFC<Props> = ({ toggleItem, menuList }) => {
+  return (
+    <ChakraMenu>
+      <MenuButton>{toggleItem}</MenuButton>
+      <MenuList>
+        {menuList.map((menuItem, i) => (
+          <>
+            <MenuItem key={i} icon={menuItem.icon}>
+              {menuItem.text}
+            </MenuItem>
+            {menuItem.underline && <MenuDivider />}
+          </>
+        ))}
+      </MenuList>
+    </ChakraMenu>
+  )
+}
