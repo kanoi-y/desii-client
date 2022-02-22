830fc594cdcce763922f2fc6eb930c8c88cbf5a4
kanoi-y <furafuratonbo@gmail.com>
2022-02-22 21:42:48 +0900

    🤡 useGetUserQueryとuseGetFavoritesQueryをhandlerに追加

--- src/mocks/factories/index.ts
+++ src/mocks/factories/index.ts
@@ -1,3 +1,4 @@
+export { favoriteFactory } from './favoriteFactory'
 export { groupFactory } from './groupFactory'
 export { postFactory } from './postFactory'
 export { userFactory } from './userFactory'
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,10 @@
 import { graphql } from 'msw'
 import { UserGroupRelation } from '~/types/generated/graphql'
-import { userGroupRelationFactory } from './factories'
+import {
+  favoriteFactory,
+  userFactory,
+  userGroupRelationFactory,
+} from './factories'
 
 export const handlers = [
   graphql.query('GetUserGroupRelations', (req, res, ctx) => {
@@ -13,6 +17,28 @@ export const handlers = [
         GetUserGroupRelations: [
           {
             ...userGroupRelationFactory(query),
+          },
+        ],
+      })
+    )
+  }),
+  graphql.query('useGetUserQuery', (req, res, ctx) => {
+    const { getUserId } = req.variables
+    return res(
+      ctx.data({
+        useGetUserQuery: {
+          ...userFactory({ id: getUserId }),
+        },
+      })
+    )
+  }),
+  graphql.query('useGetFavoritesQuery', (req, res, ctx) => {
+    const { postId } = req.variables
+    return res(
+      ctx.data({
+        useGetFavoritesQuery: [
+          {
+            ...favoriteFactory({ postId }),
           },
         ],
       })
