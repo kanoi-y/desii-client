2f65fd14bace302ce76ea8ede8e91261a6bdebb9
kanoi-y <furafuratonbo@gmail.com>
2022-02-22 19:58:28 +0900

    ✨ postFactory.tsを作成

--- src/mocks/factories/index.ts
+++ src/mocks/factories/index.ts
@@ -1,3 +1,4 @@
 export { groupFactory } from './groupFactory'
+export { postFactory } from './postFactory'
 export { userFactory } from './userFactory'
 export { userGroupRelationFactory } from './userGroupRelationFactory'
