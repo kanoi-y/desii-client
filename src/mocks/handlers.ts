b7c7c4b5a9da42875d46abb3006323013b2b3add
kanoi-y <furafuratonbo@gmail.com>
2022-02-24 16:58:59 +0900

    🤡 CreateFavoriteとDeleteFavoriteのmockを追加

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -44,4 +44,24 @@ export const handlers = [
       })
     )
   }),
+  graphql.mutation('CreateFavorite', (req, res, ctx) => {
+    const { postId } = req.variables
+    return res(
+      ctx.data({
+        createFavorite: {
+          ...favoriteFactory({ postId }),
+        },
+      })
+    )
+  }),
+  graphql.mutation('DeleteFavorite', (req, res, ctx) => {
+    const { postId } = req.variables
+    return res(
+      ctx.data({
+        DeleteFavorite: {
+          ...favoriteFactory({ postId }),
+        },
+      })
+    )
+  }),
 ]
