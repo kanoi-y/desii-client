a9678a824f37fc797cfcb148b0200c5a01b84536
kanoi-y <furafuratonbo@gmail.com>
2022-03-01 20:18:26 +0900

    🔨 fallback時はloadindを表示するように実装

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -1,7 +1,11 @@
 import { Box } from '@chakra-ui/react'
 import { GetStaticPropsContext, NextPage } from 'next'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useContext } from 'react'
 import { PostCard } from '~/components/domains/post/PostCard'
+import { BREAKPOINTS } from '~/constants'
+import { useWindowDimensions } from '~/hooks'
+import { CurrentUserContext } from '~/hooks/CurrentUserProvider'
 import { addApolloState, initializeApollo } from '~/lib/apolloClient'
 import { GET_POST_BY_ID } from '~/queries'
 import {
@@ -17,11 +21,26 @@ type Props = {
 }
 
 const PostPage: NextPage<Props> = ({ post }) => {
+  const router = useRouter()
+  const { currentUser } = useContext(CurrentUserContext)
+
+  const { width } = useWindowDimensions()
+
+  if (router.isFallback) {
+    // TODO: spinnerに変更する
+    return <div>Loading...</div>
+  }
+
   return (
     <Box p={['20px 10px', '40px 20px']}>
-      <Box mx="auto" maxW="900px">
-        <PostCard post={post} />
+      <Box mx="auto" maxW="700px">
+        <PostCard
+          post={post}
+          isBig={width > BREAKPOINTS.sm}
+          currentUserId={currentUser?.id}
+        />
       </Box>
+      <Box></Box>
     </Box>
   )
 }
@@ -29,7 +48,7 @@ const PostPage: NextPage<Props> = ({ post }) => {
 export async function getStaticPaths() {
   return {
     paths: [],
-    fallback: 'blocking',
+    fallback: true,
   }
 }
 
