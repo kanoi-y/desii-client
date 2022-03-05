9895dd759a0799fd72f4c41322689e54257ac842
kanoi-y <furafuratonbo@gmail.com>
2022-03-05 22:40:11 +0900

    ✨ Modal.tsxを作成

---
+++ src/components/parts/commons/Modal/Modal.tsx
@@ -0,0 +1,36 @@
+import {
+  Modal as ChakraModal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react'
+import { VFC } from 'react'
+
+type Props = {
+  isOpen: boolean
+  onClose: () => void
+  title: string
+  body: JSX.Element
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+}
+
+export const Modal: VFC<Props> = ({
+  isOpen,
+  onClose,
+  title,
+  body,
+  size = 'md',
+}) => {
+  return (
+    <ChakraModal size={size} isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>{body}</ModalBody>
+      </ModalContent>
+    </ChakraModal>
+  )
+}
