0177f06b8c891b8a82d93de15e95463e94fcf8ee
kanoi-y <furafuratonbo@gmail.com>
2022-03-08 22:53:17 +0900

    ✨ dashboard/posts/new.tsxを作成

---
+++ src/pages/dashboard/posts/new.tsx
@@ -0,0 +1,75 @@
+import { Box } from '@chakra-ui/react'
+import { GetServerSideProps, NextPage } from 'next'
+import { getSession } from 'next-auth/react'
+import React from 'react'
+import { initializeApollo } from '~/lib/apolloClient'
+import { GET_CURRENT_USER } from '~/queries'
+import {
+  GetCurrentUserQuery,
+  GetCurrentUserQueryVariables,
+  User,
+} from '~/types/generated/graphql'
+
+const client = initializeApollo()
+
+type Props = {
+  currentUser: User
+}
+
+const NewPostPage: NextPage<Props> = ({ currentUser }) => {
+
+  return (
+    <Box textAlign="center">
+      <p>user page</p>
+      <p>{ currentUser.name}</p>
+    </Box>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const session = await getSession(ctx)
+
+    if (!session) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: '/',
+        },
+      }
+    }
+
+    const {
+      data: { getCurrentUser },
+    } = await client.query<GetCurrentUserQuery, GetCurrentUserQueryVariables>({
+      query: GET_CURRENT_USER,
+      variables: {
+        accessToken: session.accessToken || '',
+      },
+    })
+
+    if (!getCurrentUser) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: '/',
+        },
+      }
+    }
+
+    return {
+      props: {
+        currentUser: getCurrentUser,
+      },
+    }
+  } catch (error) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+    }
+  }
+}
+
+export default NewPostPage
