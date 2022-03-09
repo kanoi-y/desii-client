2d4e10d9e0dd326ff3f3f56902bdc2bee8c95449
kanoi-y <furafuratonbo@gmail.com>
2022-03-09 21:33:03 +0900

    💄 post作成画面の見た目をざっくり作成

--- src/pages/dashboard/posts/new.tsx
+++ src/pages/dashboard/posts/new.tsx
@@ -1,7 +1,8 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Input, Textarea } from '@chakra-ui/react'
 import { GetServerSideProps, NextPage } from 'next'
 import { getSession } from 'next-auth/react'
 import React from 'react'
+import { Button, Text } from '~/components/parts/commons'
 import { initializeApollo } from '~/lib/apolloClient'
 import { GET_CURRENT_USER } from '~/queries'
 import {
@@ -17,11 +18,92 @@ type Props = {
 }
 
 const NewPostPage: NextPage<Props> = ({ currentUser }) => {
-
   return (
-    <Box textAlign="center">
-      <p>user page</p>
-      <p>{ currentUser.name}</p>
+    <Box p={['28px 10px 0', '40px 20px 0']}>
+      <Box mx="auto" maxW="700px">
+        <Box mb="32px">
+          <Box mb="12px">
+            <Text fontSize="md" isBold>
+              タイトル
+            </Text>
+          </Box>
+          <Input
+            bgColor="white.main"
+            boxShadow="0 3px 6px rgba(0, 0, 0, 0.16)"
+          />
+        </Box>
+        <Box mb="32px">
+          <Box mb="12px">
+            <Text fontSize="md" isBold>
+              本文
+            </Text>
+          </Box>
+          <Textarea
+            bgColor="white.main"
+            boxShadow="0 3px 6px rgba(0, 0, 0, 0.16)"
+            rows={10}
+          />
+        </Box>
+        <Box mb="32px">
+          <Box mb="12px">
+            <Text fontSize="md" isBold>
+              カテゴリー
+            </Text>
+          </Box>
+          <Box
+            p="8px 12px"
+            bgColor="white.main"
+            display="flex"
+            alignItems="center"
+            boxShadow="0 3px 6px rgba(0, 0, 0, 0.16)"
+            borderRadius="8px"
+            cursor="pointer"
+          >
+            <Box
+              w="100%"
+              p="16px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="16px"
+              bgColor="orange.main"
+              boxShadow="0 3px 6px rgba(0, 0, 0, 0.16)"
+              cursor="auto"
+            >
+              <Text fontSize="lg" isBold color="white.main">
+                出来ること
+              </Text>
+            </Box>
+            <Box
+              w="100%"
+              p="16px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="16px"
+            >
+              <Text fontSize="lg" isBold>
+                してほしいこと
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+        <Box mb="48px">
+          <Box mb="12px">
+            <Text fontSize="md" isBold>
+              マッチングタグ
+            </Text>
+          </Box>
+          <Input
+            bgColor="white.main"
+            boxShadow="0 3px 6px rgba(0, 0, 0, 0.16)"
+          />
+        </Box>
+        <Box display="flex" alignItems="center" justifyContent="space-evenly">
+          <Button>キャンセル</Button>
+          <Button>作成する</Button>
+        </Box>
+      </Box>
     </Box>
   )
 }
