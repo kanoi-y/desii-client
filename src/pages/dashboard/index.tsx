0c4cb8069a7de08689f447515087a55c7817aaef
kanoi-y <furafuratonbo@gmail.com>
2022-03-11 22:53:40 +0900

    ✨ index.tsxを作成

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <Box textAlign="center">
+      <p>dashboard page</p>
+    </Box>
+  )
+}
