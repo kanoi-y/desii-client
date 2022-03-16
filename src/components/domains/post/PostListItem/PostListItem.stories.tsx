d63d35dfd83c6f221a2d23e799211c5ddf4eb687
kanoi-y <furafuratonbo@gmail.com>
2022-03-16 23:34:37 +0900

    ✨ PostListItem.stories.tsxを作成

---
+++ src/components/domains/post/PostListItem/PostListItem.stories.tsx
@@ -0,0 +1,28 @@
+import { ApolloProvider } from '@apollo/client'
+import { Box } from '@chakra-ui/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+import { initializeApollo } from '~/lib/apolloClient'
+import { postFactory } from '~/mocks/factories'
+import { PostListItem } from './PostListItem'
+
+export default {
+  title: 'domains/post/PostListItem',
+  component: PostListItem,
+} as ComponentMeta<typeof PostListItem>
+
+const Template: ComponentStory<typeof PostListItem> = ({ ...args }) => {
+  const client = initializeApollo()
+  return (
+    <ApolloProvider client={client}>
+      <Box p="20px">
+        <PostListItem {...args} />
+      </Box>
+    </ApolloProvider>
+  )
+}
+
+export const DefaultPostListItem = Template.bind({})
+DefaultPostListItem.args = {
+  post: postFactory(),
+}
