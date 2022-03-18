6858e2d9892a7aab01e95786857701d357591aa1
kanoi-y <furafuratonbo@gmail.com>
2022-03-18 22:43:30 +0900

    ✨ favorites/index.tsxを作成

---
+++ src/pages/dashboard/favorites/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Favorites() {
+  return (
+    <Box textAlign="center">
+      <p>favorites page</p>
+    </Box>
+  )
+}
