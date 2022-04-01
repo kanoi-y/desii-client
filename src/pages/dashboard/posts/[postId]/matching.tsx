c85035a9b14ea176d81dd0638cfbe23039d41fb7
kanoi-y <furafuratonbo@gmail.com>
2022-04-01 22:12:48 +0900

    🔨 matching.tsxにPostCardを追加

--- src/pages/dashboard/posts/[postId]/matching.tsx
+++ src/pages/dashboard/posts/[postId]/matching.tsx
@@ -2,6 +2,10 @@ import { Box } from '@chakra-ui/react'
 import { GetServerSideProps, NextPage } from 'next'
 import { getSession } from 'next-auth/react'
 import React from 'react'
+import { PostCard } from '~/components/domains/post/PostCard'
+import { SolidIcon, Text } from '~/components/parts/commons'
+import { BREAKPOINTS } from '~/constants'
+import { useWindowDimensions } from '~/hooks'
 import { initializeApollo } from '~/lib/apolloClient'
 import { GET_CURRENT_USER, GET_POST_BY_ID } from '~/queries'
 import {
@@ -9,14 +13,45 @@ import {
   GetCurrentUserQueryVariables,
   GetPostQuery,
   GetPostQueryVariables,
+  Post,
+  User,
 } from '~/types/generated/graphql'
 
 const client = initializeApollo()
 
-const MatchingPage: NextPage = () => {
+type Props = {
+  currentUser: User
+  post: Post
+}
+
+const MatchingPage: NextPage<Props> = ({ currentUser, post }) => {
+  const { width } = useWindowDimensions()
+
   return (
-    <Box textAlign="center">
-      <p>Matching page</p>
+    <Box p={['28px 10px 0', '40px 20px 0']}>
+      <Box mx="auto" maxW="700px">
+        <PostCard
+          post={post}
+          isBig={width > BREAKPOINTS.sm}
+          currentUserId={currentUser.id}
+          isLink
+        />
+        <Box
+          display="flex"
+          alignItems="center"
+          gap="4px"
+          pb="16px"
+          m="24px 0 16px"
+          borderBottom="2px solid"
+          borderColor="secondary.light"
+        >
+          <SolidIcon icon="SOLID_HEART" color="red.main" size={36} />
+          <Text fontSize="lg" isBold>
+            この投稿にマッチした投稿
+          </Text>
+        </Box>
+        <Box></Box>
+      </Box>
     </Box>
   )
 }
@@ -66,6 +101,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     return {
       props: {
         currentUser: getCurrentUser,
+        post: getPost,
       },
     }
   } catch (error) {
