30d403b81401e9b3220f88e8edfca4e45198232b
kanoi-y <furafuratonbo@gmail.com>
2022-04-09 17:11:01 +0900

    🔨 NotificationsPageのアクセスコントロール

--- src/pages/dashboard/notifications/index.tsx
+++ src/pages/dashboard/notifications/index.tsx
@@ -1,10 +1,64 @@
 import { Box } from '@chakra-ui/react'
+import { GetServerSideProps, NextPage } from 'next'
+import { getSession } from 'next-auth/react'
 import React from 'react'
+import { initializeApollo } from '~/lib/apolloClient'
+import { GET_CURRENT_USER } from '~/queries'
+import {
+  GetCurrentUserQuery,
+  GetCurrentUserQueryVariables,
+  User,
+} from '~/types/generated/graphql'
 
-export default function NotificationsPage() {
+const client = initializeApollo()
+
+type Props = {
+  currentUser: User
+}
+
+const NotificationsPage: NextPage<Props> = ({ currentUser }) => {
   return (
     <Box textAlign="center">
-      <p>notifications page</p>
+      <p>{currentUser.name}</p>
     </Box>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const session = await getSession(ctx)
+
+    const {
+      data: { getCurrentUser },
+    } = await client.query<GetCurrentUserQuery, GetCurrentUserQueryVariables>({
+      query: GET_CURRENT_USER,
+      variables: {
+        accessToken: session?.accessToken || '',
+      },
+    })
+
+    if (!getCurrentUser) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: '/',
+        },
+      }
+    }
+
+    return {
+      props: {
+        currentUser: getCurrentUser,
+      },
+    }
+  } catch (error) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+    }
+  }
+}
+
+export default NotificationsPage
