cd720ac62b25346c2a89d4e1ca32ad2f106f2bde
kanoi-y <furafuratonbo@gmail.com>
2022-04-20 23:53:59 +0900

    🔨 GetMessagesを実装

--- src/graphql/types/Message.ts
+++ src/graphql/types/Message.ts
@@ -1,4 +1,12 @@
-import { enumType, objectType } from 'nexus'
+import {
+  arg,
+  enumType,
+  extendType,
+  nonNull,
+  objectType,
+  stringArg,
+} from 'nexus'
+import { OrderByType } from './Post'
 
 export const MessageType = enumType({
   name: 'MessageType',
@@ -20,6 +28,59 @@ export const Message = objectType({
     })
     t.nonNull.field('updatedAt', {
       type: 'DateTime',
+    })
+  },
+})
+
+export const GetMessagesQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.nonNull.field('GetMessages', {
+      type: 'Message',
+      args: {
+        targetId: nonNull(stringArg()),
+        sort: arg({
+          type: OrderByType,
+          default: 'asc',
+        }),
+      },
+      async resolve(_parent, args, ctx) {
+        if (!ctx.user) {
+          throw new Error('ログインユーザーが存在しません')
+        }
+
+        const oneOnOneRoom = await ctx.prisma.oneOnOneRoom.findUnique({
+          where: {
+            id: args.targetId,
+          },
+        })
+
+        const userGroupRelations = await ctx.prisma.userGroupRelation.findMany({
+          where: {
+            groupId: args.targetId,
+          },
+        })
+
+        if (
+          oneOnOneRoom?.memberId1 !== ctx.user.id &&
+          oneOnOneRoom?.memberId2 !== ctx.user.id &&
+          userGroupRelations.every(
+            (userGroupRelation) =>
+              ctx.user && userGroupRelation.userId !== ctx.user.id
+          )
+        ) {
+          throw new Error('所属していないルームのメッセージは閲覧できません')
+        }
+
+        return ctx.prisma.message.findMany({
+          where: {
+            targetId: args.targetId,
+          },
+          orderBy: {
+            createdAt: args.sort || 'asc',
+          },
+        })
+      },
     })
   },
 })
