5ba55ac9dc86d6e547fbb00d7e472f8a64fa5f70
kanoi-y <furafuratonbo@gmail.com>
2022-04-22 23:40:59 +0900

    🔨 GetReadManagementQueryを実装

--- src/graphql/types/ReadManagement.ts
+++ src/graphql/types/ReadManagement.ts
@@ -1,17 +1,40 @@
-import { objectType } from 'nexus'
+import { extendType, nonNull, objectType, stringArg } from 'nexus'
 
 export const ReadManagement = objectType({
   name: 'ReadManagement',
   definition(t) {
     t.nonNull.string('id')
     t.nonNull.string('targetUserId')
-    t.nonNull.string('MessageId')
+    t.nonNull.string('messageId')
     t.nonNull.boolean('isRead')
     t.nonNull.field('createdAt', {
       type: 'DateTime',
     })
     t.nonNull.field('updatedAt', {
       type: 'DateTime',
+    })
+  },
+})
+
+export const GetReadManagementQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.field('GetReadManagement', {
+      type: 'ReadManagement',
+      args: {
+        targetUserId: nonNull(stringArg()),
+        messageId: nonNull(stringArg()),
+      },
+      resolve(_parent, args, ctx) {
+        return ctx.prisma.readManagement.findUnique({
+          where: {
+            ReadManagementId: {
+              targetUserId: args.targetUserId,
+              messageId: args.messageId,
+            },
+          },
+        })
+      },
     })
   },
 })
