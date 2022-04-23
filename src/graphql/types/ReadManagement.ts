10bf208eaaf857ef6ab1008c86c7d3f281043e5e
kanoi-y <furafuratonbo@gmail.com>
2022-04-23 22:54:01 +0900

    🔨 UpdateReadManagementMutationを実装

--- src/graphql/types/ReadManagement.ts
+++ src/graphql/types/ReadManagement.ts
@@ -26,12 +26,63 @@ export const GetReadManagementQuery = extendType({
         messageId: nonNull(stringArg()),
       },
       resolve(_parent, args, ctx) {
+        if (!ctx.user) {
+          throw new Error('ログインユーザーが存在しません')
+        }
+
         return ctx.prisma.readManagement.findUnique({
           where: {
             ReadManagementId: {
               targetUserId: args.targetUserId,
               messageId: args.messageId,
             },
+          },
+        })
+      },
+    })
+  },
+})
+
+export const UpdateReadManagementMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('UpdateReadManagement', {
+      type: 'ReadManagement',
+      args: {
+        targetUserId: nonNull(stringArg()),
+        messageId: nonNull(stringArg()),
+      },
+      async resolve(_parent, args, ctx) {
+        if (!ctx.user) {
+          throw new Error('ログインユーザーが存在しません')
+        }
+
+        const readManagement = await ctx.prisma.readManagement.findUnique({
+          where: {
+            ReadManagementId: {
+              targetUserId: args.targetUserId,
+              messageId: args.messageId,
+            },
+          },
+        })
+
+        if (!readManagement) {
+          throw new Error('既読管理が存在しません')
+        }
+
+        if (ctx.user.id !== args.targetUserId) {
+          throw new Error('自分の既読管理しか更新することは出来ません')
+        }
+
+        return ctx.prisma.readManagement.update({
+          where: {
+            ReadManagementId: {
+              targetUserId: args.targetUserId,
+              messageId: args.messageId,
+            },
+          },
+          data: {
+            isRead: true,
           },
         })
       },
