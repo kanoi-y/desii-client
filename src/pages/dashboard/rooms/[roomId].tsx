d217fccdaa8a7148b42e04b38f781f3aac578a67
kanoi-y <furafuratonbo@gmail.com>
2022-04-29 20:59:39 +0900

    ✨ roomの詳細ページを作成

---
+++ src/pages/dashboard/rooms/[roomId].tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+export default function RoomPage() {
+  return (
+    <Box textAlign="center">
+      <p>room page</p>
+    </Box>
+  )
+}
