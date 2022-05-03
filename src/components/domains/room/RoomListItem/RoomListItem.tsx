3aa86df26407cc26b1e5a64d7a780143e0e21f3f
kanoi-y <furafuratonbo@gmail.com>
2022-05-03 19:13:13 +0900

    🔨 latestMessageを実装

--- src/components/domains/room/RoomListItem/RoomListItem.tsx
+++ src/components/domains/room/RoomListItem/RoomListItem.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@chakra-ui/react'
-import { VFC } from 'react'
+import { useMemo, VFC } from 'react'
 import { UserIcon } from '~/components/domains/user/UserIcon'
 import { Link, Text } from '~/components/parts/commons'
 import { Room } from '~/types/generated/graphql'
@@ -12,6 +12,36 @@ type Props = {
 
 export const RoomListItem: VFC<Props> = ({ room, currentUserId }) => {
   const displayDate = formatDistanceToNow(new Date(room.updatedAt))
+
+  const latestMessage = useMemo(() => {
+    if (!room.latestMessage) {
+      return <Text fontSize="sm">{''}</Text>
+    }
+
+    const isCreatedUser = room.latestMessage.userId === currentUserId
+
+    if (room.latestMessage.type === 'MEDIA') {
+      return (
+        <Text fontSize="sm">
+          {isCreatedUser
+            ? 'あなたが画像を送信しました'
+            : `${room.latestMessage.user.name}さんが画像を送信しました`}
+        </Text>
+      )
+    }
+
+    if (room.latestMessage.type === 'POST') {
+      return (
+        <Text fontSize="sm">
+          {isCreatedUser
+            ? 'あなたが投稿に応募しました'
+            : `${room.latestMessage.user.name}さんが投稿に応募しました`}
+        </Text>
+      )
+    }
+
+    return <Text fontSize="sm">{room.latestMessage.body}</Text>
+  }, [room.latestMessage, currentUserId])
   return (
     <Box
       display="flex"
@@ -21,16 +51,14 @@ export const RoomListItem: VFC<Props> = ({ room, currentUserId }) => {
       cursor="pointer"
       _hover={{ bgColor: 'secondary.light' }}
     >
-      <Link href={`/user/${currentUser.id}`}>
-        <UserIcon user={currentUser} size="md" />
-      </Link>
+      <UserIcon user={currentUser} size="md" isLink />
       <Box>
         <Link href={`/user/${currentUser.id}`}>
           <Text fontSize="md" isBold color="primary.main">
             {currentUser.name}
           </Text>
         </Link>
-        <Text fontSize="sm">{ room.latestMessage?.body }</Text>
+        {latestMessage}
       </Box>
       <Box ml="auto">
         <Text fontSize="xs" isBold>
