0a5bfa65bf27fadeaf7d7c2b0b3959de80179c40
kanoi-y <furafuratonbo@gmail.com>
2022-05-19 16:31:32 +0900

    🎨 修正

--- src/components/domains/message/MessageBubble/MessageBubble.tsx
+++ src/components/domains/message/MessageBubble/MessageBubble.tsx
@@ -15,8 +15,6 @@ type Props = {
   currentUserId: string
 }
 
-// TODO: messageのtypeがTEXTとPOSTとMEDIAの場合を実装
-
 export const MessageBubble: VFC<Props> = ({ message, currentUserId }) => {
   const isCreatedUser = message.userId === currentUserId
 
@@ -43,142 +41,95 @@ export const MessageBubble: VFC<Props> = ({ message, currentUserId }) => {
       (readManagement) => readManagement.isRead
     ).length || 0
 
-  if (message.type === 'TEXT') {
-    if (isCreatedUser) {
-      return (
-        <Box
-          display="flex"
-          alignItems="flex-end"
-          justifyContent="flex-end"
-          gap="4px"
-          w="100%"
-        >
-          <Box>
-            {readManagementsCount > 0 && (
-              <Text fontSize="xs" color="text.light">
-                {`既読 ${message.room.groupId ? readManagementsCount : ''}`}
-              </Text>
-            )}
-            <Text fontSize="xs" color="text.light">
-              {`${message.createdAt.getHours()}:${message.createdAt.getMinutes()}`}
-            </Text>
-          </Box>
-          <Box
-            p="8px 12px"
-            borderRadius="12px"
-            bgColor="primary.main"
-            maxW="65%"
-          >
-            <Text fontSize="md" color="white.main">
-              {message.body}
-            </Text>
-          </Box>
-        </Box>
-      )
-    }
+  if (message.type === 'POST') {
     return (
-      <Box
-        display="flex"
-        alignItems="flex-end"
-        justifyContent="flex-start"
-        gap="4px"
-        w="100%"
-      >
-        {userData?.getUser ? (
-          <UserIcon size="sm" user={userData.getUser} />
-        ) : (
-          <GuestUserIcon size="sm" />
-        )}
-        <Box maxW="65%">
-          {message.room.groupId && (
-            <Box pl="8px">
-              <Text fontSize="xs" color="text.light">
-                {message.user.name}
-              </Text>
-            </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Box maxW="360px" w="100%" p="16px 16px 8px">
+          {postData?.getPost ? (
+            <PostCard post={postData.getPost} />
+          ) : (
+            <SkeletonPostCard />
           )}
-          <Box p="8px 12px" borderRadius="12px" bgColor="white.main">
-            <Text fontSize="md">{message.body}</Text>
-          </Box>
         </Box>
-        <Text fontSize="xs" color="text.light">
-          {`${message.createdAt.getHours()}:${message.createdAt.getMinutes()}`}
-        </Text>
+        <Text fontSize="sm">{`${message.user.name}さんが投稿に応募しました`}</Text>
       </Box>
     )
   }
-  if (message.type === 'MEDIA') {
-    if (isCreatedUser) {
-      return (
-        <Box
-          display="flex"
-          alignItems="flex-end"
-          justifyContent="flex-end"
-          gap="4px"
-          w="100%"
-        >
-          <Box>
-            {readManagementsCount > 0 && (
-              <Text fontSize="xs" color="text.light">
-                {`既読 ${message.room.groupId ? readManagementsCount : ''}`}
-              </Text>
-            )}
-            <Text fontSize="xs" color="text.light">
-              {`${message.createdAt.getHours()}:${message.createdAt.getMinutes()}`}
-            </Text>
-          </Box>
-          <Box borderRadius="12px" overflow="hidden" maxW="65%">
-            <Image src={message.body} alt="画像" maxH="200px" maxW="100%" />
-          </Box>
-        </Box>
-      )
-    }
+
+  if (isCreatedUser) {
     return (
       <Box
         display="flex"
         alignItems="flex-end"
-        justifyContent="flex-start"
+        justifyContent="flex-end"
         gap="4px"
         w="100%"
       >
-        {userData?.getUser ? (
-          <UserIcon size="sm" user={userData.getUser} />
-        ) : (
-          <GuestUserIcon size="sm" />
-        )}
-        <Box maxW="65%">
-          {message.room.groupId && (
-            <Box pl="8px">
-              <Text fontSize="xs" color="text.light">
-                {message.user.name}
-              </Text>
-            </Box>
+        <Box>
+          {readManagementsCount > 0 && (
+            <Text fontSize="xs" color="text.light">
+              {`既読 ${message.room.groupId ? readManagementsCount : ''}`}
+            </Text>
           )}
-          <Box borderRadius="12px" overflow="hidden">
-            <Image src={message.body} alt="画像" maxH="200px" maxW="100%" />
-          </Box>
+          <Text fontSize="xs" color="text.light">
+            {`${message.createdAt.getHours()}:${message.createdAt.getMinutes()}`}
+          </Text>
         </Box>
-        <Text fontSize="xs" color="text.light">
-          {`${message.createdAt.getHours()}:${message.createdAt.getMinutes()}`}
-        </Text>
+        <Box
+          p={message.type === 'TEXT' ? '8px 12px' : '0px'}
+          borderRadius="12px"
+          bgColor="primary.main"
+          overflow="hidden"
+          maxW="65%"
+        >
+          {message.type === 'TEXT' ? (
+            <Text fontSize="md" color="white.main">
+              {message.body}
+            </Text>
+          ) : (
+            <Image src={message.body} alt="画像" maxH="200px" maxW="100%" />
+          )}
+        </Box>
       </Box>
     )
   }
   return (
     <Box
       display="flex"
-      flexDirection="column"
-      alignItems="center"
-      p="12px"
+      alignItems="flex-end"
+      justifyContent="flex-start"
+      gap="4px"
+      w="100%"
     >
-      <Box mb="8px" maxW="360px" w="100%">
-        {postData?.getPost ? (
-          <PostCard post={postData.getPost} />
-        ) : (
-          <SkeletonPostCard />
+      {userData?.getUser ? (
+        <UserIcon size="sm" user={userData.getUser} />
+      ) : (
+        <GuestUserIcon size="sm" />
+      )}
+      <Box maxW="65%">
+        {message.room.groupId && (
+          <Box pl="8px">
+            <Text fontSize="xs" color="text.light">
+              {message.user.name}
+            </Text>
+          </Box>
         )}
+        <Box
+          p={message.type === 'TEXT' ? '8px 12px' : '0px'}
+          borderRadius="12px"
+          bgColor="white.main"
+          overflow="hidden"
+        >
+          {message.type === 'TEXT' ? (
+            <Text fontSize="md">{message.body}</Text>
+          ) : (
+            <Image src={message.body} alt="画像" maxH="200px" maxW="100%" />
+          )}
+        </Box>
       </Box>
-      <Text fontSize="md">{`${message.user.name}さんが投稿に応募しました`}</Text>
+      <Text fontSize="xs" color="text.light">
+        {`${message.createdAt.getHours()}:${message.createdAt.getMinutes()}`}
+      </Text>
     </Box>
   )
 }
