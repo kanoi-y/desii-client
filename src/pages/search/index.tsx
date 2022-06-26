98261c7b826294201a738d2f5afc8dcbab0619cc
kanoi-y <furafuratonbo@gmail.com>
2022-06-26 23:55:08 +0900

    ✨ 検索ページを作成

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+export default function SearchPage() {
+  return (
+    <Box textAlign="center">
+      <p>search page</p>
+    </Box>
+  )
+}
