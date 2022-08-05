9efb9b8a95b1e2e97fa32209ca4b8b69357e2825
kanoi-y <furafuratonbo@gmail.com>
2022-08-05 12:52:06 +0900

    🔨 getFavoritesのtestを実装

--- src/graphql/types/Favorite/__tests__/getFavorites.test.ts
+++ src/graphql/types/Favorite/__tests__/getFavorites.test.ts
@@ -1,58 +1,72 @@
-import { Favorite, Post, PostCategory, User } from '@prisma/client'
-import { Context, createMockContext, MockContext } from '../../../context'
+import { Favorite, Post, User } from '@prisma/client'
+import { prisma } from '../../../../lib/prisma'
+import { resetDatabase } from '../../../logics'
 import { getFavoritesResolver } from '../resolver'
 
+// TODO: DBに接続してテストする
 describe('getFavorites', () => {
-  let mockCtx: MockContext
-  let ctx: Context
+  let user: User
+  let post: Post
+  let favorite: Favorite & {
+    createdUser: User
+    post: Post
+  }
 
-  beforeEach(() => {
-    mockCtx = createMockContext()
-    ctx = mockCtx as unknown as Context
+  beforeAll(async () => {
+    user = await prisma.user.create({
+      data: {
+        name: 'name',
+        email: 'email',
+        image: 'image',
+      },
+    })
+    post = await prisma.post.create({
+      data: {
+        title: 'title',
+        content: 'content',
+        category: 'GIVE_ME',
+        isPrivate: false,
+        createdUserId: user.id,
+      },
+    })
+    favorite = await prisma.favorite.create({
+      data: {
+        createdUserId: user.id,
+        postId: post.id,
+      },
+      include: {
+        createdUser: true,
+        post: true,
+      },
+    })
+  })
+
+  afterAll(async () => {
+    await resetDatabase()
+    await prisma.$disconnect()
+  })
+
+  const spy = jest.spyOn(prisma.favorite, 'findMany')
+
+  test('not found', async () => {
+    const res = await getFavoritesResolver({
+      sort: null,
+      createdUserId: 'createdUserId',
+      postId: 'postId',
+    })
+
+    expect(spy).toHaveBeenCalled()
+    expect(res).toEqual([])
   })
 
   test('成功', async () => {
-    const favorite: Favorite & {
-      post: Post
-      createdUser: User
-    } = {
-      id: 'favoriteId',
-      createdUserId: 'userId',
-      postId: 'postId',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      createdUser: {
-        id: 'userId',
-        name: 'userName',
-        description: 'description',
-        email: 'email',
-        image: 'image',
-        accessToken: 'accessToken',
-        emailVerified: null,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      post: {
-        id: 'postId',
-        title: 'title',
-        content: 'content',
-        category: 'GIVE_ME' as PostCategory,
-        createdUserId: 'createdUserId',
-        isPrivate: false,
-        groupId: 'groupId',
-        bgImage: 'images/Desii_bgImage.png',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    }
-    mockCtx.prisma.favorite.findMany.mockResolvedValue([favorite])
+    const res = await getFavoritesResolver({
+      sort: null,
+      createdUserId: user.id,
+      postId: post.id,
+    })
 
-    await expect(
-      getFavoritesResolver(
-        {},
-        { createdUserId: 'userId', postId: 'postId', sort: 'desc' },
-        ctx
-      )
-    ).resolves.toEqual([favorite])
+    expect(spy).toHaveBeenCalled()
+    expect(res).toEqual([favorite])
   })
 })
