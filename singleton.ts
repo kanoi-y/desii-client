07093b47c6259bc9207bdd842c5941caf6bfb164
kanoi-y <furafuratonbo@gmail.com>
2022-09-23 23:07:47 +0900

    ✨ singleton.tsを作成

---
+++ singleton.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
+
+import { prisma } from './src/lib/prisma'
+
+jest.mock('./src/lib/prisma', () => ({
+  __esModule: true,
+  prisma: mockDeep<PrismaClient>(),
+}))
+
+beforeEach(() => {
+  mockReset(prismaMock)
+})
+
+export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
